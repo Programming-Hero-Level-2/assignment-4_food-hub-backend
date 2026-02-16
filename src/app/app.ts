0a9e8c954734c1683aae5863f22d69b9cc0d3454
Naiemul Islam <naiemulislam.dev@gmail.com>
2026-02-17 01:47:04 +0600

    refactor: refactor utility function and resolve some small issues

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { applyMiddleware } from './middleware';
+import { globalErrorHandler } from '../utils/globalErrorHandler';
+import { ApiError } from '../utils/ApiError';
 
 const app = express();
 
 applyMiddleware(app);
+
 app.use('/api/v1', (await import('./routes')).default);
-// app.use(globalErrorHandler);
+
+app.use((_req: Request, _res: Response, next: NextFunction) =>
+  next(new ApiError(404, 'Route not found'))
+);
+
+app.use(globalErrorHandler);
 
 export { app };
